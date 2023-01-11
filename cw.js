8b2ee6993aa8c50f89d823e54aefc1f4e21b47fb
Kindle <1715640772@qq.com>
2023-01-11 22:13:41 +0800

    css变动

--- cw.js
+++ cw.js
@@ -6,4 +6,4 @@
 
 // importScripts('https://lib.baomitu.com/clientworker/3.0.0-beta-3/dist/cw.js')
 // importScripts('https://cdn.jsdelivr.net/npm/clientworker@3.0.0-beta-4')
-importScripts("https://cdn.jsdelivr.net/npm/clientworker@3.0.0-beta-4/dist/cw.js")
+importScripts("https://cdn.chuqis.com/npm/clientworker@3.0.0-beta-4/dist/cw.js")
